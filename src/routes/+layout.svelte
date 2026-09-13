3b9869ce457039874c6d4f0c202a13db7afcf749
sebbe1231 <sebbeb04@gmail.com>
2026-09-13 17:02:15 +0200

    yea

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	import "bootstrap/dist/js/bootstrap.bundle.min.js"
+    import "bootstrap/dist/css/bootstrap.min.css"
+
 	let { children } = $props();
 </script>
 
